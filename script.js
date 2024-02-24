f6386131359c6f0d8f45ef40e15e2457a647d8c7
SilentCaMXMF <pedroocalado@gmail.com>
2024-02-24 19:51:33 +0000

    last code for app to work,still bootcamping...

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  if (input === 0) {
+    return "0";
+  } 
+  
+  else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
